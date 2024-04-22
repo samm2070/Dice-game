818bc87eafd75f926b6a9885abffefb134efc419
sam <sammm2767@gmail.com>
2024-04-22 23:32:13 +0530

    added switching player

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 
 const score0EL = document.getElementById('score--0')
 const score1EL = document.getElementById('score--1')
 const diceEL = document.querySelector('.dice')
+const current0El = document.getElementById('current--0')
+const current1El = document.getElementById('current--1');
+
 
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -14,6 +19,10 @@ score0EL.textContent = 0;
 score1EL.textContent = 0;
 diceEL.classList.add('hidden')
 
+score = [0, 0]
+let activePlayer = 0;
+let currentScore = 0;
+
 
 // rolling dice functionality
 
@@ -23,6 +32,28 @@ btnRoll.addEventListener('click', function () {
 
     diceEL.src = `dice-${dice}.png`
     diceEL.classList.remove('hidden')
+
+
+    if (dice !== 1) {
+        currentScore += dice;
+
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+
+    }
+    else {
+        currentScore = 0;
+
+        player0El.classList.toggle('player--active')
+        player1El.classList.toggle('player--active')
+
+        activePlayer = activePlayer === 0 ? 1 : 0;
+
+
+
+
+    }
+
+
 
 
 
